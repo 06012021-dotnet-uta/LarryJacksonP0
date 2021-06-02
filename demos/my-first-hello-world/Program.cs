214573160a74c46e0331df67ab232156045cd435
LarryProgrammer <60943960+LarryProgrammer@users.noreply.github.com>
2021-06-02 12:13:38 -0400

    Demos

---
+++ demos/my-first-hello-world/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace my_first_hello_world
+{
+    class Program {
+
+        static void Main(string [] args) {
+
+            Console.WriteLine("Welcome to my 'Hello World' console!");
+    
+            
+            Console.WriteLine();
+
+           /* int myNum = 15;
+            Console.WriteLine("\t{0} {1}\n" myNum); 
+            
+            Console.WriteLine($"This is string interpolation! The number is {myNum}"); */
+
+            Console.WriteLine("Tell me your name.");/* Asking the user for their first name */
+            Console.Write("Type your first name: ");/* prompt the user to enter their first name */
+            
+            /* Gets the user input of their name*/
+            string myFirstName; 
+            myFirstName = Console.ReadLine();
+            
+            /* This adds a space between each questions */
+            Console.WriteLine();
+
+            Console.WriteLine("How old are you? ");/* Asking the user how old they are */
+            
+            /* Gets the user input of their age*/
+            Console.Write("Type your age: ");
+            string myAge = Console.ReadLine();/* Receives the user input of their age */
+
+            Console.WriteLine();/* Adds a space between the prompt questions/ responses and the display at the bottom */
+            Console.WriteLine($"Hello, {myFirstName} you are {myAge} years old.");
+        }
+    }
+}
